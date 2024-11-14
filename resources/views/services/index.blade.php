2a7bebf923ad8165957fdb508f2f6f5b15a6fdf2
anucha1993 <108377127+anucha1993@users.noreply.github.com>
2024-11-14 14:04:07 +0700

    6554

--- resources/views/services/index.blade.php
+++ resources/views/services/index.blade.php
@@ -88,8 +88,8 @@
                             </div>
                             <a href="javascript:void(0);" data-id="{{ 1 }}" data-modal-target="import-modal"
                                 data-modal-toggle="import-modal"
-                                class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 modal-trigger">
-                                รายละเอียดบริการ
+                                class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 modal-trigger">
+                                ดูรายละเอียด
                             </a>
                         </div>
                     </div>
